f2113d9b8251b5ab6f298f2ecc021c58cd2d150a
Michael Hansen <hansen.mike@gmail.com>
2020-03-14 14:40:26 -0400

    Better logging in on_message

--- rhasspyhomeassistant_hermes/__init__.py
+++ rhasspyhomeassistant_hermes/__init__.py
@@ -197,6 +197,7 @@ class HomeAssistantHermesMqtt:
                 _LOGGER.debug("Intent handling disabled")
         except Exception:
             _LOGGER.exception("on_message")
+            _LOGGER.error("%s %s", msg.topic, msg.payload)
 
     def publish(self, message: Message, **topic_args):
         """Publish a Hermes message to MQTT."""
